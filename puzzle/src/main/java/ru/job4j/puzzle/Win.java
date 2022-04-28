5bc336c29cb2ffcd6c0a837a5537df9dff5bded4
VladimirKozemaslov <kvs@intelins.ru>
2022-04-28 15:55:32 +0500

    2. Сокобан. Тестовое задание. [#75283]
    Добавил методы согласно задания.

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,34 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
-        return rsl;
+        return isHorizontalWin(board) || isVerticalWin(board);
+    }
+
+    public static boolean isHorizontalWin(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] != 1) {
+                    break;
+                }
+                if (j == board[i].length - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean isVerticalWin(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[j][i] != 1) {
+                    break;
+                }
+                if (j == board.length - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
